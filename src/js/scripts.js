f38582591cbbfd8529d7c336ffc5a54ab694a550
Matheus Wisniewski <matheus.maw@gmail.com>
2018-07-11 04:42:20 -0300

    First commit

--- src/js/scripts.js
+++
@@ -1,7 +0,0 @@
-global.jQuery = require('jquery');
-bootstrap = require('bootstrap');
-mustache = require('mustache');
-
-jQuery(document).ready(function($) {
-    $("#craze-carousel").crazeCarousel();
-})()
